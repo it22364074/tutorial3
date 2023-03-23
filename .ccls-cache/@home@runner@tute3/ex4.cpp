ac80c86e5a6037c470f27fedc3b506a0f1f423e1
it22364074 <127375108+it22364074@users.noreply.github.com>
2023-03-23 06:22:45 +0000

    tute03

---
+++ .ccls-cache/@home@runner@tute3/ex4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+  void print(int len, int wth);
+  void input(int len, int wth);
+
+int main() {
+   int length = 10, width = 5;
+   input(length, width);
+   print(length, width);
+ return 0;
+}
+
+void print(int len, int wth) {
+ cout << "Length : " << len
+ << ", Width : " << wth << endl;
+}
+void input(int len, int wth){
+	
+	cout << "Enter length : " ;
+	cin>> len;
+	cout << "Enter width : " ;
+    cin>> len;
+}
